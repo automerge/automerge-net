8316bc4687a48096bd5d3194312e9c6f4d6a4b15
Martin Kleppmann <martin@kleppmann.com>
2018-11-05 15:42:02 +0000

    Timer to make document changes

--- client.js
+++ client.js
@@ -5,18 +5,32 @@ const HOST = '127.0.0.1'
 const PORT = 9876
 const docSet = new Automerge.DocSet()
 
+// Print out the document whenever it changes
 docSet.registerHandler((docId, doc) => {
   console.log(`[${docId}] ${JSON.stringify(doc)}`)
 })
 
+// Make a change to the document every 3 seconds
+setInterval(() => {
+  let doc = docSet.getDoc('example')
+  if (doc) {
+    doc = Automerge.change(doc, doc => {
+      doc.clientNum = (doc.clientNum || 0) + 1
+    })
+    docSet.setDoc('example', doc)
+  }
+}, 3000)
+
 const socket = new net.Socket()
 let connection
 
+// Connecting to a TCP port
 socket.connect(PORT, HOST, () => {
   console.log(`[${HOST}:${PORT}] connected`)
   connection = new Connection(docSet, socket)
 })
 
+// Receiving data from the server
 socket.on('data', (data) => {
   if (!(data instanceof Buffer)) {
     data = Buffer.from(data, 'utf8')
--- server.js
+++ server.js
@@ -4,18 +4,31 @@ const net = require('net')
 const HOST = '0.0.0.0'
 const PORT = 9876
 
+// Initialise an example document
 const docSet = new Automerge.DocSet()
 const initDoc = Automerge.change(Automerge.init(), doc => doc.hello = 'Hi!')
 docSet.setDoc('example', initDoc)
 
+// Print out the document whenever it changes
 docSet.registerHandler((docId, doc) => {
   console.log(`[${docId}] ${JSON.stringify(doc)}`)
 })
 
+// Make a change to the document every 3 seconds
+setInterval(() => {
+  let doc = docSet.getDoc('example')
+  doc = Automerge.change(doc, doc => {
+    doc.serverNum = (doc.serverNum || 0) + 1
+  })
+  docSet.setDoc('example', doc)
+}, 3000)
+
+// This function calls every time a client connects to the server socket
 function handler(socket) {
   console.log(`[${socket.remoteAddress}:${socket.remotePort}] connected`)
   const connection = new Connection(docSet, socket)
 
+  // Receiving data from the client
   socket.on('data', (data) => {
     if (!(data instanceof Buffer)) {
       data = Buffer.from(data, 'utf8')
@@ -32,5 +45,6 @@ function handler(socket) {
   })
 }
 
+// Listen on a TCP port
 net.createServer(handler).listen(PORT, HOST)
 console.log(`Listening on ${HOST}:${PORT}`)
